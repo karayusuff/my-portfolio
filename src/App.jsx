04a1d56fec290d021f37148e8aa7b6a31c605f57
Yusuf Kara <yusufkara5562@gmail.com>
2025-02-08 07:25:26 -0500

    Add fade-in feature instead of loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useState } from "react";
+// import { useState, useEffect } from "react";
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
 import Navbar from "./components/Navbar";
-import Loading from "./components/Loading";
+// import Loading from "./components/Loading";
 import Skills from "./components/Skills";
 
+// function App() {
+//   const [loading, setLoading] = useState(true);
+
+//   return (
+//     <div>
+//       {loading ? (
+//         <Loading setLoading={setLoading} />
+//       ) : (
+//         <>
+//           <Navbar />
+//           <Home />
+//           <About />
+//           <Skills />
+//           <Projects />
+//           <Contact />
+//           <Footer />
+//         </>
+//       )}
+//     </div>
+//   );
+// }
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
   return (
-    <div>
-      {loading ? (
-        <Loading setLoading={setLoading} />
-      ) : (
-        <>
-          <Navbar />
-          <Home />
-          <About />
-          <Skills />
-          <Projects />
-          <Contact />
-          <Footer />
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
